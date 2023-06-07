074d9978b4f5d77a975bebdd5d5580c49b647d76
Derian Melvin <derianmelvin88@gmail.com>
2023-06-07 19:20:35 +0700

    Create ImageListData

---
+++ src/data/ImageListData.tsx
@@ -0,0 +1,42 @@
+export const imageListData = [
+  {
+    imgName: "Trees and Soil",
+    imgLink:
+      "https://images.pexels.com/photos/288093/pexels-photo-288093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Snowfield",
+    imgLink:
+      "https://images.pexels.com/photos/953182/pexels-photo-953182.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Green Forest",
+    imgLink:
+      "https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Bridge Under The Clouds",
+    imgLink:
+      "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Snow Capped Mountains",
+    imgLink:
+      "https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Aurora Borealis",
+    imgLink:
+      "https://images.pexels.com/photos/1933317/pexels-photo-1933317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Small Jellyfish",
+    imgLink:
+      "https://images.pexels.com/photos/1687678/pexels-photo-1687678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    imgName: "Snow Covered Trees",
+    imgLink:
+      "https://images.pexels.com/photos/3509971/pexels-photo-3509971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
