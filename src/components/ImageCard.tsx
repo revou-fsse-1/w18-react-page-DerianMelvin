db9d9aca0510545926818914afc3bbe2b8ffa2f3
Derian Melvin <derianmelvin88@gmail.com>
2023-06-08 20:08:05 +0700

    Adjust props & optimize code visibility

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,38 +3,40 @@ import { useState } from "react";
 type ImageCardProps = {
   imgName: string;
   imgLink: string;
+  searchFilter: string;
   setFavourites: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const ImageCard = ({
   imgName,
   imgLink,
+  searchFilter,
   setFavourites,
 }: ImageCardProps) => {
   const [toggleFavourite, setToggleFavourite] = useState<boolean>(false);
 
-  const handleDisplayedIcon = () => {
-    return toggleFavourite ? (
+  const handleFavouriteIcon = () => {
+    const toggleClassName = toggleFavourite
+      ? "absolute bottom-2 right-2 cursor-pointer fill-rose-600 transition-all scale-125"
+      : "absolute bottom-2 right-2 cursor-pointer fill-white transition-all";
+
+    return (
       <svg
         width="30"
         height="25"
         viewBox="0 0 33 31"
         onClick={toggleFavouriteImage}
-        className="absolute bottom-2 right-2 cursor-pointer fill-rose-600 transition-all scale-125"
-      >
-        <path d="M16.5 30.6938L14.1438 28.5488C5.775 20.96 0.25 15.9388 0.25 9.8125C0.25 4.79125 4.1825 0.875 9.1875 0.875C12.015 0.875 14.7288 2.19125 16.5 4.255C18.2712 2.19125 20.985 0.875 23.8125 0.875C28.8175 0.875 32.75 4.79125 32.75 9.8125C32.75 15.9388 27.225 20.96 18.8562 28.5488L16.5 30.6938Z" />
-      </svg>
-    ) : (
-      <svg
-        width="30"
-        height="25"
-        viewBox="0 0 33 31"
-        onClick={toggleFavouriteImage}
-        className="absolute bottom-2 right-2 cursor-pointer fill-white transition-all"
+        className={toggleClassName}
       >
         <path d="M16.5 30.6938L14.1438 28.5488C5.775 20.96 0.25 15.9388 0.25 9.8125C0.25 4.79125 4.1825 0.875 9.1875 0.875C12.015 0.875 14.7288 2.19125 16.5 4.255C18.2712 2.19125 20.985 0.875 23.8125 0.875C28.8175 0.875 32.75 4.79125 32.75 9.8125C32.75 15.9388 27.225 20.96 18.8562 28.5488L16.5 30.6938Z" />
       </svg>
     );
+  };
+
+  const handleClassNameBySearchFilter = () => {
+    return imgName.toLowerCase().includes(searchFilter.toLowerCase())
+      ? "w-48 h-48 flex flex-col relative"
+      : "hidden";
   };
 
   const toggleFavouriteImage = () => {
@@ -48,16 +50,20 @@ export const ImageCard = ({
   };
 
   return (
-    <div className="w-48 h-48 flex flex-col relative">
-      <h3 className="w-full h-20 absolute px-3 pt-2 pb-10 truncate rounded-2xl bg-gradient-to-b from-slate-950">
+    <div className={handleClassNameBySearchFilter()}>
+      <h3
+        onDoubleClick={toggleFavouriteImage}
+        className="w-full h-20 absolute px-3 pt-2 pb-10 truncate rounded-2xl bg-gradient-to-b from-slate-950"
+      >
         {imgName}
       </h3>
       <img
         src={imgLink}
         alt={imgName}
         className="w-full h-full object-cover rounded-2xl"
+        onDoubleClick={toggleFavouriteImage}
       />
-      {handleDisplayedIcon()}
+      {handleFavouriteIcon()}
     </div>
   );
 };
