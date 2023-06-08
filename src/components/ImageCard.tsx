2c1d5a15da8cb2362ee59dba5a30fdd8103674a4
Derian Melvin <derianmelvin88@gmail.com>
2023-06-08 15:30:37 +0700

    Adjust ImageCard & ImageList

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,20 +1,63 @@
-import favourite from "../assets/favourite.svg";
+import { useState } from "react";
 
 type ImageCardProps = {
   imgName: string;
   imgLink: string;
+  setFavourites: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export const ImageCard = ({ imgName, imgLink }: ImageCardProps) => {
+export const ImageCard = ({
+  imgName,
+  imgLink,
+  setFavourites,
+}: ImageCardProps) => {
+  const [toggleFavourite, setToggleFavourite] = useState<boolean>(false);
+
+  const handleDisplayedIcon = () => {
+    return toggleFavourite ? (
+      <svg
+        width="30"
+        height="25"
+        viewBox="0 0 33 31"
+        onClick={toggleFavouriteImage}
+        className="absolute bottom-2 right-2 cursor-pointer fill-rose-600 transition-all scale-125"
+      >
+        <path d="M16.5 30.6938L14.1438 28.5488C5.775 20.96 0.25 15.9388 0.25 9.8125C0.25 4.79125 4.1825 0.875 9.1875 0.875C12.015 0.875 14.7288 2.19125 16.5 4.255C18.2712 2.19125 20.985 0.875 23.8125 0.875C28.8175 0.875 32.75 4.79125 32.75 9.8125C32.75 15.9388 27.225 20.96 18.8562 28.5488L16.5 30.6938Z" />
+      </svg>
+    ) : (
+      <svg
+        width="30"
+        height="25"
+        viewBox="0 0 33 31"
+        onClick={toggleFavouriteImage}
+        className="absolute bottom-2 right-2 cursor-pointer fill-white transition-all"
+      >
+        <path d="M16.5 30.6938L14.1438 28.5488C5.775 20.96 0.25 15.9388 0.25 9.8125C0.25 4.79125 4.1825 0.875 9.1875 0.875C12.015 0.875 14.7288 2.19125 16.5 4.255C18.2712 2.19125 20.985 0.875 23.8125 0.875C28.8175 0.875 32.75 4.79125 32.75 9.8125C32.75 15.9388 27.225 20.96 18.8562 28.5488L16.5 30.6938Z" />
+      </svg>
+    );
+  };
+
+  const toggleFavouriteImage = () => {
+    if (!toggleFavourite) {
+      setToggleFavourite((prev) => !prev);
+      setFavourites((prev) => prev + 1);
+    } else {
+      setToggleFavourite((prev) => !prev);
+      setFavourites((prev) => prev - 1);
+    }
+  };
+
   return (
     <div className="w-48 h-48 flex flex-col relative">
-      <h3 className="w-full h-20 absolute px-3 pt-2 pb-10 truncate rounded-2xl bg-gradient-to-b from-slate-950">{imgName}</h3>
+      <h3 className="w-full h-20 absolute px-3 pt-2 pb-10 truncate rounded-2xl bg-gradient-to-b from-slate-950">
+        {imgName}
+      </h3>
       <img
         src={imgLink}
         alt={imgName}
         className="w-full h-full object-cover rounded-2xl"
       />
-      <img src={favourite} alt="favourite this photo" className="w-7 object-contain absolute bottom-2 right-2" />
+      {handleDisplayedIcon()}
     </div>
   );
 };
--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -2,13 +2,27 @@ import { useState } from "react";
 import { imageListData } from "../data/ImageListData";
 import { ImageCard } from "./ImageCard";
 
-export const ImageList = () => {
-  const [image, setImage] = useState(imageListData);
+type ImageListData = {
+  imgName: string;
+  imgLink: string;
+}[];
+
+type ImageListProps = {
+  setFavourites: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const ImageList = ({ setFavourites }: ImageListProps) => {
+  const [image, setImage] = useState<ImageListData>(imageListData);
 
   return (
     <main className="w-full grow px-10 py-10 mx-auto flex flex-wrap gap-4 justify-center items-center place-content-start backdrop-blur-md backdrop-brightness-90">
       {image.map((img) => (
-        <ImageCard imgName={img.imgName} imgLink={img.imgLink} />
+        <ImageCard
+          key={img.imgLink}
+          imgName={img.imgName}
+          imgLink={img.imgLink}
+          setFavourites={setFavourites}
+        />
       ))}
     </main>
   );
