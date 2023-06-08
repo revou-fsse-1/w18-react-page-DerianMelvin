97286ddc213933da7014a16be6a246bcd0987b10
Derian Melvin <derianmelvin88@gmail.com>
2023-06-08 15:28:57 +0700

    Adjust Header component - Create FavouritesCounter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,20 @@
 import logo from "../assets/logo.svg";
-import favourite from "../assets/favourite.svg";
+import { FavouritesCounter } from "./FavouritesCounter";
 
-export const Header = () => {
+type HeaderProps = {
+  favourites: number;
+};
+
+export const Header = ({ favourites }: HeaderProps) => {
   return (
     <header className="flex flex-col justify-center items-center backdrop-blur-md backdrop-brightness-[0.6]">
-      <div className="flex self-end px-3 py-2 bg-black bg-opacity-40 rounded-bl-2xl">
-        <img
-          src={favourite}
-          alt="total favourites"
-          className="w-8 object-contain"
-        />
-        <p className="ml-1 text-xl">12</p>
-      </div>
+      <FavouritesCounter favourites={favourites} />
 
       <div className="flex flex-col justify-center items-center mb-7 sm:flex-row">
         <img src={logo} alt="logo" className="w-16 object-contain" />
         <h1 className="text-5xl text-center sm:ml-4">Gallery of Earth</h1>
       </div>
-      
+
       <div className="w-full flex items-center justify-center">
         <label htmlFor="searchBar" className="sr-only">
           searchBar
