168129c368fb99bd1fea4a73bd3ddcd29887d1d3
Derian Melvin <derianmelvin88@gmail.com>
2023-06-08 15:27:08 +0700

    Create favourites state - Add props to Header & ImageList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { ImageList } from "./components/ImageList";
 
 function App() {
+  const [favourites, setFavourites] = useState<number>(0);
+
   return (
     <div className="min-h-screen flex flex-col justify-between bg-[url('./assets/background.jpg')] bg-no-repeat bg-cover bg-fixed text-gray-200">
-      <Header />
-      <ImageList />
+      <Header favourites={favourites} />
+      <ImageList setFavourites={setFavourites} />
       <Footer />
     </div>
   );
