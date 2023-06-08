041099c9d77c9246af3383bce6944ca3958a6ef8
Derian Melvin <derianmelvin88@gmail.com>
2023-06-08 20:02:09 +0700

    Create SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,30 @@
+type SearchBarProps = {
+  searchFilter: string;
+  setSearchFilter: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const SearchBar = ({
+  searchFilter,
+  setSearchFilter,
+}: SearchBarProps) => {
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchFilter(e.target.value);
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <label htmlFor="searchBar" className="sr-only">
+        searchBar
+      </label>
+      <input
+        type="text"
+        name="searchBar"
+        id="searchBar"
+        placeholder="Search for..."
+        value={searchFilter}
+        onChange={(e) => handleSearchInput(e)}
+        className="w-4/5 px-4 py-2 mb-7 border-transparent rounded-xl text-black placeholder-slate-500 focus:outline-none"
+      />
+    </div>
+  );
+};
