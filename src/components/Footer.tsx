bbe1ffb077c72d1b55783aa51e7aeb62a7a90fe7
Derian Melvin <derianmelvin88@gmail.com>
2023-06-08 13:50:02 +0700

    Create Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="px-5 pt-6 flex flex-col justify-center items-center gap-6 text-center backdrop-blur-md backdrop-brightness-50">
+      <h2 className="text-2xl">Find out more on the Earth's vast beauty</h2>
+      <button className="px-10 py-3 text-xl rounded-lg text-white bg-green-600">Subscribe</button>
+      <span className="mt-5 text-gray-400">© 2023</span>
+    </footer>
+  );
+};
